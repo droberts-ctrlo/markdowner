3ae0fb01c967090961b8f0399aa55f19dc9a1ebf
Dave Roberts <d.roberts@ctrlo.com>
2024-02-07 15:22:06 +0000

    Moved to pure JS solution

--- projects/markdowner/src/lib/markdowner.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MarkdownerPipe } from './markdowner.pipe';
-
-describe('MarkdownerPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MarkdownerPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
